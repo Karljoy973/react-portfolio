55a0365b3a6430b01ae19c83ae430962122d85c9
Karl <karljoyeux@gmail.com>
2025-08-19 17:27:38 +0200

    6-f-4-2025-08-19

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
+/// <reference types="vitest/config" />
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import {resolve} from "path"
+import { resolve } from "path"
+import { configDefaults} from "vitest/config"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), !process.env.VITEST &&  reactRouter(), tsconfigPaths()],
    resolve: {
       alias: {
       "@": resolve(__dirname, "./app"),
     },
   },
+  test: {
+    ...configDefaults,
+    environment: "happy-dom"
+    
+  },
+  
 });
