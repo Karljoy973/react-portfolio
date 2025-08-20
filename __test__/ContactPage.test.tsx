bb4b16264ea90dc3252d9e4da192502a56c31c0c
Karl <karljoyeux@gmail.com>
2025-08-20 22:17:43 +0200

    4-f-2-2025-08-20

--- __test__/ContactPage.test.tsx
+++ __test__/ContactPage.test.tsx
@@ -1,8 +1,8 @@
 import { describe, expect, test } from "vitest";
 import { cleanup, render } from "@testing-library/react";
 import { screen } from "@testing-library/dom";
-import { HomePage } from "@/home/index";
 import { afterEach, beforeEach } from "node:test";
+import ContactPage from "@/contact";
 
 /**!SECTION
  * // Lazy load de composants internes
@@ -16,12 +16,12 @@ afterEach(() => {
   cleanup();
 });
 
-describe("all of the tests related to the HomePage ", () => {
-  test("verifie que le premier element apres le main est une section et qu elle contient le test id home-page ", () => {
-    render(<HomePage />);
+describe("all of the tests related to the ContactPage ", () => {
+  test("verifie que le premier element apres le main est une section et qu elle contient le test id contact-page ", () => {
+    render(<ContactPage />);
     screen.getAllByRole("generic");
     const section = document.getElementsByTagName("section");
     expect(section).not.toBeUndefined();
-    expect(section[0].getAttribute("data-testid")).toBe("home-page");
+    expect(section[0].getAttribute("data-testid")).toBe("contact-page");
   });
 });
---
+++ app/contact/index.tsx
@@ -0,0 +1,7 @@
+const ContactPage = () => {
+    return ( <section className="w-[100%] flex" data-testid="contact-page">
+        <h1>Contact Page</h1>
+      </section> );
+}
+ 
+export default ContactPage;
