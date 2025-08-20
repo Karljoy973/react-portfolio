44ca3757018bbfb5412dbf4cf545db30dd287459
Karl <karljoyeux@gmail.com>
2025-08-20 22:17:43 +0200

    4-f-2-2025-08-20

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,12 @@
+import { HomePage } from "@/home";
 import type { Route } from "./+types/home";
-import { HomePage } from "../home/index";
 
 export function meta(meta: Route.MetaArgs) {
-  return [
-    { title: "Page principale" },
-    { name: "description", content: "Bienvenue sur mon portefolio" },
-  ];
+	return [
+		{ title: "Page principale" },
+		{ name: "description", content: "Bienvenue sur mon portefolio" },
+	];
 }
 export default function Home() {
-  return <HomePage />;
+	return <HomePage />;
 }
