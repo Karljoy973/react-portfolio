f3970bf009ea0cc646b79cd71250d953e863a18d
Karl <karljoyeux@gmail.com>
2025-08-21 16:02:51 +0200

    14-f-15-2025-08-21

---
+++ app/cv/index.tsx
@@ -0,0 +1,7 @@
+const CVPage = () => {
+    return ( <section className="w-[100%] flex" data-testid="cv-page">
+        <h1>Voici le détail de mes compétences et de mes experiences</h1>
+      </section> );
+}
+ 
+export default CVPage;
